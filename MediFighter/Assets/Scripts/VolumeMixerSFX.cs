aea425034b5dadfb46c7f2a0225a735b2d06edaa
Benjamin Hunsicker <benhunsicker1@gmail.com>
2021-12-12 20:20:22 -0500

    finished sfx mixer and fixed shield

--- MediFighter/Assets/Scripts/VolumeMixerSFX.cs
+++ MediFighter/Assets/Scripts/VolumeMixerSFX.cs
@@ -10,13 +10,13 @@ public class VolumeMixerSFX : MonoBehaviour
 
     private void Start()
     {
-        mixer.SetFloat("MusicVolume", Mathf.Log10(PlayerPrefs.GetFloat("volsfx", 0.3f)) * 20);
+        mixer.SetFloat("SFX", Mathf.Log10(PlayerPrefs.GetFloat("volsfx", 0.3f)) * 20);
         gameObject.GetComponent<Slider>().value = PlayerPrefs.GetFloat("volsfx", 0.3f);
     }
 
     public void SetLevel (float sliderValue)
     {
-        mixer.SetFloat("MusicVolume", Mathf.Log10(sliderValue) * 20);
+        mixer.SetFloat("SFX", Mathf.Log10(sliderValue) * 20);
         PlayerPrefs.SetFloat("volsfx", sliderValue);
     }
 }
