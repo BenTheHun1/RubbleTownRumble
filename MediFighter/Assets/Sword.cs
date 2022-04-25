f01fb05f31a33cbddbcf7fef661f728c1919e50d
Ben <benhunsicker1@gmail.com>
2022-04-25 14:13:47 -0400

    Fix shield and player height

--- MediFighter/Assets/Sword.cs
+++ MediFighter/Assets/Sword.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR;
 
 public class Sword : MonoBehaviour
 {
+   
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,7 @@ public class Sword : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(gameObject.GetComponent<Rigidbody>().angularVelocity.magnitude.ToString("N0") + " " + gameObject.GetComponent<Rigidbody>().velocity.magnitude.ToString("N0"));
+        //Debug.Log(gameObject.GetComponent<Rigidbody>().angularVelocity.magnitude.ToString("N0") + " " + gameObject.GetComponent<Rigidbody>().velocity.magnitude.ToString("N0"));
+        
     }
 }
