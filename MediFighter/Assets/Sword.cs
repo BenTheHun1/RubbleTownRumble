b8fdec15fab44f849b471a97c3bc626c70bf3701
Ben <benhunsicker1@gmail.com>
2022-04-25 14:59:20 -0400

    sword tweaks

--- MediFighter/Assets/Sword.cs
+++ MediFighter/Assets/Sword.cs
@@ -2,13 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class Sword : MonoBehaviour
 {
-   
+    private InputDevice rCon;
+    public static XRBaseController leftHand, rightHand;
+    public ActionBasedController left, right;
+
     // Start is called before the first frame update
     void Start()
     {
+        /*var inputDevices = new List<InputDevice>();
+        InputDevices.GetDevicesWithCharacteristics(InputDeviceCharacteristics.Right, inputDevices);
+        rCon = inputDevices[0];
+        rCon.TryGetFeatureValue()*/
+        leftHand = left;
+        rightHand = right;
         
     }
 
@@ -16,6 +26,6 @@ public class Sword : MonoBehaviour
     void Update()
     {
         //Debug.Log(gameObject.GetComponent<Rigidbody>().angularVelocity.magnitude.ToString("N0") + " " + gameObject.GetComponent<Rigidbody>().velocity.magnitude.ToString("N0"));
-        
+        Debug.Log(right.transform.position);
     }
 }
